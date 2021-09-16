9e6498b87c8560f359c3d7783330a66f510048a6
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-16 15:49:35 +0200

    aggiunto testo e immagini dinamiche con vue js

--- js/index.js
+++ js/index.js
@@ -4,13 +4,25 @@ const root = new Vue(
         el: "#root",
         data: {
             bird: {
-                name: "Bird",
-                type: ['01', '02', '03', '04', '05'],
-                dir: "images/"
+                index: 0,
+                src: "images/01.jpg",
+                filesName: ['01', '02', '03', '04', '05'],
+                name: "Bird 1"
             }
         },
         methods: {
-
+            next: function () {
+                this.bird.index++;
+                if (this.bird.index >= this.bird.filesName.length) this.bird.index = 0;
+                this.bird.src = "images/" + this.bird.filesName[this.bird.index] + ".jpg";
+                this.bird.name = "Bird " + (this.bird.index + 1);
+            },
+            prev: function () {
+                this.bird.index--;
+                if (this.bird.index < 0) this.bird.index = this.bird.filesName.length - 1;
+                this.bird.src = "images/" + this.bird.filesName[this.bird.index] + ".jpg";
+                this.bird.name = "Bird " + (this.bird.index + 1);
+            }
         },
     }
 );
