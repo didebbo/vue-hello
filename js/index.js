5547553ff846e67157787dbffee59ae3a5ef03fc
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-16 18:08:57 +0200

    .

--- js/index.js
+++ js/index.js
@@ -5,23 +5,25 @@ const root = new Vue(
         data: {
             bird: {
                 index: 0,
-                src: "images/" + this.bird.filesName[0] + ".jpg",
-                name: "Bird" + this.bird.filesName[0],
-                filesName: ['01', '02', '03', '04', '05']
+                filesName: ['01', '02', '03', '04', '05'],
             },
+        },
+        created: function () {
+            this.bird.name = "Bird " + this.bird.filesName[0];
+            this.bird.src = "images/" + this.bird.filesName[0] + ".jpg";
         },
         methods: {
             next: function () {
                 this.bird.index++;
                 if (this.bird.index >= this.bird.filesName.length) this.bird.index = 0;
+                this.bird.name = "Bird " + this.bird.filesName[this.bird.index];
                 this.bird.src = "images/" + this.bird.filesName[this.bird.index] + ".jpg";
-                this.bird.name = "Bird " + (this.bird.index + 1);
             },
             prev: function () {
                 this.bird.index--;
                 if (this.bird.index < 0) this.bird.index = this.bird.filesName.length - 1;
+                this.bird.name = "Bird " + this.bird.filesName[this.bird.index];
                 this.bird.src = "images/" + this.bird.filesName[this.bird.index] + ".jpg";
-                this.bird.name = "Bird " + (this.bird.index + 1);
             }
         },
     }
