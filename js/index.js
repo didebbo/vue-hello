5ce96881ffbc23b1ce3505a7620eab5f7a0458a4
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-16 14:51:12 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Vue
+const root = new Vue(
+    {
+        el: "#root",
+        data: {
+            bird: {
+                name: "Bird",
+                type: ['01', '02', '03', '04', '05'],
+                dir: "images/"
+            }
+        },
+        methods: {
+
+        },
+    }
+);
